10527b687b448026cbac3aa8b20e8978b3182929
Adrien Roussel <Roussel-adrien@outlook.fr>
2017-10-05 13:59:03 +0200

    Correct PEP8 Guide

--- medias.py
+++ medias.py
@@ -1,14 +1,17 @@
 import webbrowser
 
 
-class Movie():
+# Definethe movies class.
 
+class Movie():
+    # Function for differents parameters.
     def __init__(self, movie_title, movie_story, poster_img, trailer_youtube):
         self.title = movie_title
         self.story = movie_story
         self.poster_img_url = poster_img
         self.trailer_youtube_url = trailer_youtube
 
+    # Function to show trailer.
+
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-    
